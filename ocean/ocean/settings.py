6e1684ac72f8f101154312d69213747867e62c01
renfake <fake.ren@gmail.com>
2013-01-04 16:10:42 +0800

    open admin, and set templates dir

--- ocean/ocean/settings.py
+++ ocean/ocean/settings.py
@@ -1,10 +1,11 @@
 # Django settings for ocean project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('admin', 'admin@example.com'),
 )
 
 MANAGERS = ADMINS
@@ -67,6 +68,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+
 )
 
 # List of finder classes that know how to find static files in
@@ -116,7 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
